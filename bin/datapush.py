d7b2a3cbe747fc0f4a8b3d836c7100841413acf0
Brett Adams <brett.whynot@gmail.com>
2023-06-11 16:21:08 +1000

    Initial

---
+++ bin/datapush.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+import requests
+import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", 'lib'))
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option
+
+@Configuration()
+class datapushCommand(StreamingCommand):
+    host=Option(
+        doc='''
+        **Syntax:** **host=***<hostname>*
+        **Description:** The hostname (and port) to send data to.''',
+        require=True
+    )
+    token=Option(
+        doc='''
+        **Syntax:** **token=***<string>*
+        **Description:** The HEC token.''',
+        require=True
+    )
+
+    def stream(self, events):
+        data = [{
+            "index": 'test', #event["index"],
+            "host": event["host"],
+            "source": event["source"],
+            "sourcetype": event["sourcetype"],
+            "time": event["_time"],
+            "event": event["_raw"]
+        } for event in events]
+        if data:
+            requests.post(
+                f"https://{self.host}/services/collector/event",
+                data=json.dumps(data,separators=(",",":")),
+                headers = {"Authorization": f"Splunk {self.token}"}
+            )
+        yield from events
+
+dispatch(datapushCommand, sys.argv, sys.stdin, sys.stdout, __name__)
+
